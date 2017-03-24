78116b0401ec192d8e4abd3e5b0f27deedb3f883
Silas Reinagel <silas.reinagel@gmail.com>
2017-03-23 21:40:24 -0700

    Added Caller Patience Decay

--- MegaBuy/Calls/Caller.cs
+++ MegaBuy/Calls/Caller.cs
@@ -1,22 +1,44 @@
-﻿
-using System;
+﻿using System;
 using MegaBuy.Calls.Rules;
 using MonoDragons.Core.Engine;
 
 namespace MegaBuy.Calls
 {
-    public class Caller : IAutomaton
+    public sealed class Caller : IAutomaton
     {
         public CallerPatience Patience = CallerStartingPatience.New;
-        private int _patienceLossRateMs = 100;
+
+        private readonly int _patienceLossRateMs;
+        private int _gracePeriods;
 
         private double _elapsedMs;
+
+        public Caller(int patienceLossRateMs)
+        {
+            _patienceLossRateMs = patienceLossRateMs;
+            _gracePeriods = 3;
+        }
 
         public void Update(TimeSpan delta)
         {
             _elapsedMs += delta.TotalMilliseconds;
+            UpdatePatience();
             if (Patience.Value == 0)
                 World.Publish(new CallFailed());
+        }
+
+        private void UpdatePatience()
+        {
+            if (_elapsedMs < _patienceLossRateMs) return;
+
+            _elapsedMs -= _patienceLossRateMs;
+            if (_gracePeriods > 0)
+            {
+                _gracePeriods--;
+                return;
+            }
+
+            Patience.Reduce();
         }
     }
 }
---
+++ MegaBuy/Calls/Rules/CallerPatienceDecay.cs
@@ -0,0 +1,17 @@
+﻿using MonoDragons.Core.Engine;
+
+namespace MegaBuy.Calls.Rules
+{
+    public static class CallerPatienceDecay
+    {
+        private static readonly Map<int, int> _dayDecayMs = new Map<int, int>
+        {
+            {1, 5000},
+        };
+
+        public static int CallDecayForDay(int day)
+        {
+            return _dayDecayMs[day];
+        }
+    }
+}
