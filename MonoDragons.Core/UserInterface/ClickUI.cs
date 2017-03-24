7bf7e52458608bc3b1f2e4982b78aecbd1953552
Silas Reinagel <silas.reinagel@gmail.com>
2017-03-23 22:26:39 -0700

    Added ClickUi, and ImageButton2

---
+++ MonoDragons.Core/UserInterface/ClickUI.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoDragons.Core.Engine;
+
+namespace MonoDragons.Core.UserInterface
+{
+    public sealed class ClickUI : IAutomaton
+    {
+        private static readonly ClickableUIElement None = new NoneClickableUIElement();
+
+        private List<ClickableUIElement> _elements = new List<ClickableUIElement>();
+
+        private ClickableUIElement _currentElement = None;
+        private bool _wasClicked;
+
+        public void Add(ClickableUIElement element)
+        {
+            _elements.Add(element);
+            _elements = _elements.OrderByDescending(x => x.Layer).ToList();
+        }
+
+        public void Remove(ClickableUIElement element)
+        {
+            _elements.Remove(element);
+        }
+
+        public void Update(TimeSpan delta)
+        {
+            var mouse = Mouse.GetState();
+            var newElement = GetElement(mouse.Position);
+            if (newElement != _currentElement)
+                ChangeActiveElement(newElement);
+            else if (WasMouseReleased(mouse))
+                OnReleased();
+            else if (mouse.LeftButton == ButtonState.Pressed)
+                OnPressed();
+        }
+
+        private void OnPressed()
+        {
+            _currentElement.OnPressed();
+            _wasClicked = true;
+        }
+
+        private void OnReleased()
+        {
+            _currentElement.OnReleased();
+            _wasClicked = false;
+        }
+
+        private void ChangeActiveElement(ClickableUIElement newElement)
+        {
+            _currentElement.OnExitted();
+            _wasClicked = false;
+            _currentElement = newElement;
+            _currentElement.OnEntered();
+        }
+
+        private bool WasMouseReleased(MouseState mouse)
+        {
+            return _wasClicked && mouse.LeftButton == ButtonState.Released && _currentElement.Area.Contains(mouse.Position);
+        }
+
+        private ClickableUIElement GetElement(Point mousePosition)
+        {
+            var element = _elements.FirstOrDefault(x => x.Area.Contains(mousePosition));
+            return element ?? None;
+        }
+    }
+}
---
+++ MonoDragons.Core/UserInterface/ImageButton2.cs
@@ -0,0 +1,50 @@
+﻿using MonoDragons.Core.Engine;
+using MonoDragons.Core.PhysicsEngine;
+
+namespace MonoDragons.Core.UserInterface
+{
+    public sealed class ImageButton2 : ClickableUIElement, IVisual
+    {
+        private readonly string _basic;
+        private readonly string _hover;
+        private readonly string _pressed;
+        private readonly Transform2 _transform;
+        
+        private string _current;
+
+        public ImageButton2(string basic, string hover, string pressed, Transform2 transform) 
+            : base(10, transform.ToRectangle())
+        {
+            _basic = basic;
+            _hover = hover;
+            _pressed = pressed;
+            _transform = transform;
+            _current = _basic;
+        }
+
+        public void Draw(Transform2 parentTransform)
+        {
+            World.Draw(_current, parentTransform + _transform);
+        }
+
+        public override void OnEntered()
+        {
+            _current = _hover;
+        }
+
+        public override void OnExitted()
+        {
+            _current = _basic;
+        }
+
+        public override void OnPressed()
+        {
+            _current = _pressed;
+        }
+
+        public override void OnReleased()
+        {
+            _current = _basic;
+        }
+    }
+}
