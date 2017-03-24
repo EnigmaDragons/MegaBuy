71b9e5af3a4a92cf01248f2e145b1dc398294477
noah reinagel <noah.r9@gmail.com>
2017-03-23 22:50:31 -0700

    Added Menu Stuff

---
+++ MegaBuy/Scene/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.PhysicsEngine;
+using MonoDragons.Core.UserInterface;
+
+namespace MegaBuy.Scene
+{
+    public class Menu : IScene
+    {
+        private ImageButton _btn;
+
+        public void Init()
+        { 
+            _btn = new ImageButton("Images/Menu/button-default", "Images/Menu/button-hover", "Images/Menu/button-pressed", new Transform2(new Vector2(800 - 100, 750 - 22), new Size2(200, 44)), () => World.NavigateToScene("Room"));
+        }
+
+        public void Update(TimeSpan delta)
+        {
+            _btn.Update(delta);
+        }
+
+        public void Draw()
+        {
+            World.Draw("Images/Menu/menu", new Transform2(new Vector2(0, 0), new Size2(1600, 900)));
+            UI.DrawCenteredWithOffset("Images/Screen/mega-buy", new Vector2(0, -300));
+            _btn.Draw(new Transform2(new Vector2(0, 0)));
+            UI.DrawText("Start", new Vector2(772, 735), Color.White);
+        }
+    }
+}
