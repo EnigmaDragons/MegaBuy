2f9a97601f31791b4445bd3aa2f53670eabb79e0
Tim Reinagel <tmr2_handbell@yahoo.com>
2017-03-28 22:17:03 -0700

    Temp

---
+++ MegaBuy/Calls/Conversation Pieces/Nouns.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaBuy.Calls.Conversation_Pieces
+{
+    public class Nouns
+    {
+        public static Dictionary<VerbAttribute, string[]> nouns = new Dictionary<string, VerbAttribute[]> {
+            { "Flying Car", new VerbAttribute[] { VerbAttribute.CanRun, VerbAttribute.CanTurnOnOrOff } }
+        };
+    }
+}
