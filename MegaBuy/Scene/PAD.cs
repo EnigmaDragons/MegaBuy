a8fb64795ba5e6edd7af8c50f1c4d682d7de7c07
Silas Reinagel <silas.reinagel@gmail.com>
2017-03-30 23:11:52 -0700

    Added Food and Video call buttons

--- MegaBuy/Scene/PAD.cs
+++ MegaBuy/Scene/PAD.cs
@@ -12,6 +12,8 @@ namespace MegaBuy.Scene
     public sealed class PAD : IScene
     {
         private ImageButton _leave;
+        private IconButton _food;
+        private IconButton _calls;
         private ClickUI _ui;
 
         private CallApp _app;
@@ -22,11 +24,21 @@ namespace MegaBuy.Scene
                 "Images/Icons/poweroff",
                 "Images/Icons/poweroff-hover",
                 "Images/Icons/poweroff-hover",
-                new Transform2(new Vector2(52, 750), new Size2(96, 96)),
+                new Transform2(new Vector2(52, 750), new Size2(90, 90)),
                 () => World.NavigateToScene("Room"));
+
+            var iconRect = new Rectangle(new Point(55, 10), new Point(90, 90));
+
+            _food = new IconButton("Images/Icons/burger", iconRect, new Rectangle(new Point(0, 0), new Point(200, 110)),
+                Color.LightBlue, Color.Blue, Color.DarkBlue);
+
+            _calls = new IconButton("Images/Icons/video-call", iconRect, new Rectangle(new Point(0, 110), new Point(200, 110)),
+                Color.LightBlue, Color.Blue, Color.DarkBlue);
 
             _ui = new ClickUI();
             _ui.Add(_leave);
+            _ui.Add(_food);
+            _ui.Add(_calls);
 
             _app = new CallApp(new CallGenerater(CallCenterPosition.Referrer).GenerateCall());
         }
@@ -40,6 +52,8 @@ namespace MegaBuy.Scene
         public void Draw()
         {
             World.Draw("Images/Screen/screen2", new Transform2(new Vector2(0, 0), new Size2(1600, 900)));
+            _food.Draw(Transform2.Zero);
+            _calls.Draw(Transform2.Zero);
             _leave.Draw(Transform2.Zero);
             _app.Draw(new Transform2(new Vector2(200, 0), new Size2(0, 0)));
         }
---
+++ MonoDragons.Core/UserInterface/IconButton.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.PhysicsEngine;
+
+namespace MonoDragons.Core.UserInterface
+{
+    public sealed class IconButton : ClickableUIElement, IVisual
+    {
+        private readonly string _icon;
+        private readonly Color _defaultColor;
+        private readonly Color _hover;
+        private readonly Color _pressed;
+        private readonly Rectangle _buttonArea;
+        private readonly Rectangle _iconArea;
+
+        private Color _color;
+
+        public IconButton(string icon, Rectangle iconArea, Rectangle buttonArea, Color defaultColor, Color hover, Color pressed) 
+            : base(10, buttonArea)
+        {
+            _icon = icon;
+            _iconArea = iconArea;
+            _iconArea.Offset(buttonArea.Location);
+            _defaultColor = defaultColor;
+            _hover = hover;
+            _pressed = pressed;
+            _color = _defaultColor;
+            _buttonArea = buttonArea;
+        }
+
+        public override void OnEntered()
+        {
+            _color = _hover;
+        }
+
+        public override void OnExitted()
+        {
+            _color = _defaultColor;
+        }
+
+        public override void OnPressed()
+        {
+            _color = _pressed;
+        }
+
+        public override void OnReleased()
+        {
+            _color = _hover;
+        }
+
+        public void Draw(Transform2 parentTransform)
+        {
+            var buttonArea = new Rectangle(_buttonArea.Location, _buttonArea.Size);
+            buttonArea.Offset(parentTransform.Location);
+            World.DrawRectangle(_buttonArea, _color);
+            var iconArea = new Rectangle(_iconArea.Location, _iconArea.Size);
+            iconArea.Offset(parentTransform.Location);
+            World.Draw(_icon, iconArea);
+        }
+    }
+}
