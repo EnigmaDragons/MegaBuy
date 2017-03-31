2b34136feba49ac136f1384306bbb7e52efeb463
Tomas <tomizechsterson@gmail.com>
2017-03-30 21:32:11 -0700

    Adding notifications
    Notifications for Money depsited into player account, rent paid, and rent increased now being published. We also have a Remove method in the PlayerAccount. Not sure what exactly this might do yet, so leaving the NoImplementedException in there for now
    
    Resolves #19

--- MegaBuy/Money/PlayerAccount.cs
+++ MegaBuy/Money/PlayerAccount.cs
@@ -1,9 +1,30 @@
-﻿
+﻿using MonoDragons.Core.Engine;
+
 namespace MegaBuy.Money
 {
-    public sealed class PlayerAccount : SimpleAccount
+    public sealed class PlayerAccount : IAccount, IAmount
     {
-        public PlayerAccount() 
-            : base(500) { }
+        private decimal _amount;
+
+        public PlayerAccount(decimal initial)
+        {
+            _amount = initial;
+        }
+
+        public void Add(IAmount amount)
+        {
+            _amount += amount.Amount();
+            World.Publish(new MoneyDeposited());
+        }
+
+        public void Remove(IAmount amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public decimal Amount()
+        {
+            return _amount;
+        }
     }
 }
