df1533cb19c10f9947bbe43c8c0dca3c8f486ff3
Silas Reinagel <silas.reinagel@gmail.com>
2017-04-13 21:40:04 -0700

    Added more product. Made some changes to the organization of call data

---
+++ MegaBuy/Calls/Conversation Pieces/Problems.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace MegaBuy.Calls.Conversation_Pieces
+{
+    public class Verbs
+    {
+        public static Dictionary<NounOperations, string> verbs = new Dictionary<NounOperations, string>
+        {
+            { NounOperations.CanRun, "run" },
+            { NounOperations.CanTurnOnOrOff, "turn on" },
+            { NounOperations.CanTurnOnOrOff, "turn off" },
+            { NounOperations.Jams, "always jams" },
+            { NounOperations.IsDefective, "arrived with a defect" },
+            { NounOperations.IsBuggy, "has tons of known bugs" },
+            { NounOperations.HasSpyware, "infected my system with spyware" },
+            { NounOperations.RunsSlowly, "infected my system with spyware" },
+        };
+
+        public static string[] canNotSynonyms = new string[] { "can't", "fails to", "is unable to" };
+    }
+
+    public enum NounOperations
+    {
+        CanRun,
+        CanTurnOnOrOff,
+        Jams,
+        IsDefective,
+        TerribleExperience,
+        IsBuggy,
+        HasSpyware,
+        RunsSlowly
+    }
+}
