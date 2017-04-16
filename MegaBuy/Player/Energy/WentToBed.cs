3b06d58fb305002ef5eefc21e7d53ee51ea00787
Silas Reinagel <silas.reinagel@gmail.com>
2017-04-15 19:59:41 -0700

    Added Sleep transition

---
+++ MegaBuy/Player/Energy/WentToBed.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MegaBuy.Player.Energy
+{
+    public sealed class WentToBed
+    {
+        public int HoursToSleep { get; }
+
+        public WentToBed(int hoursToSleep)
+        {
+            HoursToSleep = hoursToSleep;
+        }
+    }
+}
