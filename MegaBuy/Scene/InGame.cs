7b32df1ae284f8dfaa515d6c7ade27fd22be0975
noah reinagel <noah.r9@gmail.com>
2017-04-15 17:37:49 -0700

    Added Pad Animation

--- MegaBuy/Scene/InGame.cs
+++ MegaBuy/Scene/InGame.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Drawing.Design;
 using MegaBuy.Apartment;
 using MegaBuy.Money;
 using MegaBuy.Pads;
 using MegaBuy.Player;
 using MegaBuy.Temp;
 using MegaBuy.Time;
+using Microsoft.Xna.Framework;
 using MonoDragons.Core.Engine;
 using MonoDragons.Core.EventSystem;
 using MonoDragons.Core.PhysicsEngine;
@@ -20,24 +22,29 @@ namespace MegaBuy.Scene
         private Pad _pad;
         private Clock _clock;
 
-        private bool _isPadVisible;
+        private bool _isPadOpen;
         private ApartmentMap _map;
         private PlayerCharacter _player;
+        private int _padLocation;
 
         public void Init()
         {
+            _padLocation = 900;
             _clock = GameState.Clock;
             _clickUi.Add(_branch);
             _overlay = new Overlay();
             _branch.Add(_overlay.Branch);
             _pad = new Pad(_branch);
             GameState.Pad = _pad;
-            World.Subscribe(EventSubscription.Create<PadOpened>(x => _isPadVisible = true, this));
-            World.Subscribe(EventSubscription.Create<PadClosed>(x => _isPadVisible = false, this));
+            World.Subscribe(EventSubscription.Create<PadOpened>(x => _isPadOpen = true, this));
+            World.Subscribe(EventSubscription.Create<PadClosed>(x => _isPadOpen = false, this));
         }
 
         public void Update(TimeSpan delta)
         {
+            _padLocation = _isPadOpen
+                ? (int) Math.Max(_padLocation - delta.TotalMilliseconds, 0)
+                : (int) Math.Min(_padLocation + delta.TotalMilliseconds, 900);
             _clock.Update(delta);
             _clickUi.Update(delta);
             _overlay.Update(delta);
@@ -46,8 +53,7 @@ namespace MegaBuy.Scene
 
         public void Draw()
         {
-            if(_isPadVisible)
-                _pad.Draw(Transform2.Zero);
+            _pad.Draw(new Transform2(new Vector2(0, _padLocation)));
             _overlay.Draw(Transform2.Zero);
         }
     }
