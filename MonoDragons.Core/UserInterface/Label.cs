94abaa89aa418c4842f343fe52d1ef9002b4f335
noah reinagel <noah.r9@gmail.com>
2017-04-20 23:05:11 -0700

    Finished Implementing Thought UI

--- MonoDragons.Core/UserInterface/Label.cs
+++ MonoDragons.Core/UserInterface/Label.cs
@@ -2,6 +2,9 @@
 using MonoDragons.Core.Engine;
 using MonoDragons.Core.PhysicsEngine;
 using System;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using MonoDragons.Core.Memory;
 
 namespace MonoDragons.Core.UserInterface
 {
@@ -9,9 +12,10 @@ namespace MonoDragons.Core.UserInterface
     {
         private readonly ColoredRectangle _background = new ColoredRectangle();
 
+        private string _text = "";
+
         public string Font { get; set; } = "Fonts/Arial";
         public Color TextColor { get; set; } = Color.White;
-        public string Text { get; set; } = "";
 
         public Transform2 Transform
         {
@@ -25,6 +29,12 @@ namespace MonoDragons.Core.UserInterface
             set { _background.Color = value; }
         }
 
+        public string Text
+        {
+            get { return _text; }
+            set { _text = WrapText(Resources.Load<SpriteFont>(Font), value, _background.Transform.Size.Width); }
+        }
+
         public void Draw(Transform2 parentTransform)
         {
             _background.Draw(parentTransform);
@@ -34,6 +44,29 @@ namespace MonoDragons.Core.UserInterface
         public void Dispose()
         {
             _background.Dispose();
+        }
+
+        private string WrapText(SpriteFont font, string text, int maxWidth)
+        {
+            var words = text.Split(' ');
+            var sb = new StringBuilder();
+            var lineWidth = 0f;
+            var spaceWidth = font.MeasureString(" ").X;
+            foreach (var word in words)
+            {
+                var size = font.MeasureString(word);
+                if (lineWidth + size.X < maxWidth)
+                {
+                    sb.Append(word + " ");
+                    lineWidth += size.X + spaceWidth;
+                }
+                else
+                {
+                    sb.Append("\n" + word + " ");
+                    lineWidth = size.X + spaceWidth;
+                }
+            }
+            return sb.ToString();
         }
     }
 }
