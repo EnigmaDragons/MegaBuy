ad994d14725c1e5320ba21c0d2d598f52bcd9eb7
Silas Reinagel <silas.reinagel@gmail.com>
2017-05-09 21:37:01 -0700

    Added 8 Referrer Level 3 call scripts

---
+++ MegaBuy/Calls/Options/CallResolutionOption.cs
@@ -0,0 +1,24 @@
+﻿using MegaBuy.Calls.Events;
+using MegaBuy.Calls.Rules;
+using MonoDragons.Core.Engine;
+
+namespace MegaBuy.Calls.Options
+{
+    public sealed class CallResolutionOption : ICallOption
+    {
+        private readonly CallResolution _resolution;
+
+        public string Description { get; }
+
+        public CallResolutionOption(CallResolution resolution, string description)
+        {
+            _resolution = resolution;
+            Description = description;
+        }
+
+        public void Go()
+        {
+            World.Publish(new CallResolved(_resolution));
+        }
+    }
+}
--- MegaBuy/MegaBuyCorporation/JobRoles/Referrer/ReferrerCalls.cs
+++ MegaBuy/MegaBuyCorporation/JobRoles/Referrer/ReferrerCalls.cs
@@ -4,23 +4,23 @@ using System.Linq;
 using MegaBuy.Calls;
 using MegaBuy.Calls.Conversation_Pieces;
 using MegaBuy.Calls.Messages;
+using MegaBuy.Calls.Options;
 using MegaBuy.Calls.Rules;
-using MegaBuy.JobRoles.Referrer.Options;
 using MonoDragons.Core.Common;
 
-namespace MegaBuy.JobRoles.Referrer
+namespace MegaBuy.MegaBuyCorporation.JobRoles.Referrer
 {
     public class ReferrerCalls
     {
         private static readonly List<ICallOption> Level1Options = new List<ICallOption>
         {
-            new ReferToInfo(),
-            new ReferToReturns(),
-            new ReferToTroubleshooting(),
-            new EscalateCall(),
+            new CallResolutionOption(CallResolution.ReferToInfo, "Info"),
+            new CallResolutionOption(CallResolution.ReferToReturns, "Returns"),
+            new CallResolutionOption(CallResolution.ReferToTroubleshooting, "Troubleshooting"),
+            new CallResolutionOption(CallResolution.EscalateCall, "Escalate"),
         };
         
-        private static readonly List<Func<Call>> _level1Calls = new List<Func<Call>> {
+        private static readonly List<Func<Call>> Level1Calls = new List<Func<Call>> {
             () => CreateLvl1((c, s) => c.CallerSays("I want to return this dumb " + s.Product + "!"), CallResolution.ReferToReturns),
             () => CreateLvl1((c, s) => c.CallerSays(s.Product + " needs to be returned. It " + Problems.description[s.Problem] + "."), CallResolution.ReferToReturns),
             () => CreateLvl1((c, s) => c.CallerSays("I need help. My " + s.Product + " " + Problems.description[s.Problem] + "."), CallResolution.ReferToTroubleshooting),
@@ -39,15 +39,15 @@ namespace MegaBuy.JobRoles.Referrer
 
         private static readonly List<ICallOption> Level2Options = new List<ICallOption>
         {
-            new ReferToOrders(),
-            new ReferToTroubleshooting(),
-            new ReferToInfo(),
-            new ReferToReturns(),
-            new ReferToCareers(),
-            new EscalateCall(),
+            new CallResolutionOption(CallResolution.ReferToOrders, "Orders"),
+            new CallResolutionOption(CallResolution.ReferToTroubleshooting, "Troubleshooting"),
+            new CallResolutionOption(CallResolution.ReferToInfo, "Info"),
+            new CallResolutionOption(CallResolution.ReferToReturns, "Returns"),
+            new CallResolutionOption(CallResolution.ReferToCareers, "Careers"),
+            new CallResolutionOption(CallResolution.EscalateCall, "Escalate"),
         };
 
-        private static readonly List<Func<Call>> _level2Calls = _level1Calls.Union(new Func<Call>[]
+        private static readonly List<Func<Call>> Level2Calls = Level1Calls.Union(new List<Func<Call>>
         {
             () => CreateLvl2((c, s) => c.CallerSays("I wish to apply for the new Senior Vice Product Executive position."), CallResolution.ReferToCareers),
             () => CreateLvl2((c, s) => c.CallerSays("Do you have any job openings?"), CallResolution.ReferToCareers),
@@ -58,6 +58,34 @@ namespace MegaBuy.JobRoles.Referrer
             () => CreateLvl2((c, s) => c.CallerSays($"When will you get more {s.Product} in stock?"), CallResolution.ReferToInfo),
             () => CreateLvl2((c, s) => c.CallerSays($"I just moved. I need to change my delivery address"), CallResolution.ReferToOrders),
         }).ToList();
+
+        private static readonly List<ICallOption> Level3Options = new List<ICallOption>
+        {
+            new CallResolutionOption(CallResolution.ReferToOrders, "Orders"),
+            new CallResolutionOption(CallResolution.ReferToTroubleshooting, "Troubleshooting"),
+            new CallResolutionOption(CallResolution.ReferToInfo, "Info"),
+            new CallResolutionOption(CallResolution.ReferToReturns, "Returns"),
+            new CallResolutionOption(CallResolution.ReferToCareers, "Careers"),
+            new CallResolutionOption(CallResolution.ReferToFeedback, "Feedback"),
+            new CallResolutionOption(CallResolution.ReferToAccounting, "Accounting"),
+            new CallResolutionOption(CallResolution.ReferToRecommendations, "Recommendations"),
+            new CallResolutionOption(CallResolution.ReferToLegal, "Legal"),
+            new CallResolutionOption(CallResolution.ReferToGeneralist, "Generalist"),
+        };
+
+        private static readonly List<Func<Call>> Level3Calls = new List<Func<Call>>
+        {
+            () => CreateLvl3((c, s) => c.CallerSays("Do you have any new music that I would be interested in?"), CallResolution.ReferToRecommendations),
+            () => CreateLvl3((c, s) => c.CallerSays("I need help picking out a toy for my little boy"), CallResolution.ReferToRecommendations),
+            () => CreateLvl3((c, s) => c.CallerSays("My company relocated our headquarters, I need to update our payment address"), CallResolution.ReferToAccounting),
+            () => CreateLvl3((c, s) => c.CallerSays("I have a question about an invoice discrepancy"), CallResolution.ReferToAccounting),
+            () => CreateLvl3((c, s) => c.CallerSays("I would like to lodge a complaint about a customer support person"), CallResolution.ReferToFeedback),
+            () => CreateLvl3((c, s) => c.CallerSays("Your new Essentials marketing campaign is corrupting children."), CallResolution.ReferToFeedback),
+            () => CreateLvl3((c, s) => c.CallerSays("Can you help me schedule a corporate party?"), CallResolution.ReferToGeneralist),
+            () => CreateLvl3((c, s) => c.CallerSays("Let me speak with your lawyers"), CallResolution.ReferToLegal),
+            // @ todo #1 Create 8 more Referrer Level 3 call scripts
+
+        };
 
         private static Call CreateLvl1(Action<Script, CallScenario> scriptBuilder, CallResolution correctOption)
         {
@@ -75,7 +103,15 @@ namespace MegaBuy.JobRoles.Referrer
             return new Call(scenario.Caller, script, correctOption, Level2Options);
         }
 
-        private static readonly List<string> _introductions = new List<string>()
+        private static Call CreateLvl3(Action<Script, CallScenario> scriptBuilder, CallResolution correctOption)
+        {
+            var scenario = CallScenarioFactory.Create(JobRole.ReferrerLevel3);
+            var script = InitScript();
+            scriptBuilder(script, scenario);
+            return new Call(scenario.Caller, script, correctOption, Level3Options);
+        }
+
+        private static readonly List<string> Introductions = new List<string>()
         {
             "You've reached MegaBuy, how may I direct your call?",
             "Hello there, how can I help you today?",
@@ -85,17 +121,22 @@ namespace MegaBuy.JobRoles.Referrer
 
         private static Script InitScript()
         {
-            return new Script {{CallRole.Player, _introductions.Random()}};
+            return new Script {{CallRole.Player, Introductions.Random()}};
         }
 
         public static Call NewLevel1Call()
         {
-            return _level1Calls.Random().Invoke();
+            return Level1Calls.Random().Invoke();
         }
 
         public static Call NewLevel2Call()
         {
-            return _level2Calls.Random().Invoke();
+            return Level2Calls.Random().Invoke();
+        }
+
+        public static Call NewLevel3Call()
+        {
+            return Level3Calls.Random().Invoke();
         }
     }
 }
