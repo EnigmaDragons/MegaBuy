5602ede15b9c71e844329c4d95696f91ab3288e8
noah reinagel <noah.r9@gmail.com>
2017-05-16 22:11:21 -0700

    Fixed Purchases Bug

--- MegaBuy/Jobs/ReturnSpecialist/ReturnSpecialistUI.cs
+++ MegaBuy/Jobs/ReturnSpecialist/ReturnSpecialistUI.cs
@@ -1,7 +1,10 @@
-﻿using MegaBuy.Pads.Apps;
+﻿using MegaBuy.Calls;
+using MegaBuy.Calls.Events;
+using MegaBuy.Pads.Apps;
 using MegaBuy.UIs;
 using Microsoft.Xna.Framework;
 using MonoDragons.Core.Engine;
+using MonoDragons.Core.EventSystem;
 using MonoDragons.Core.PhysicsEngine;
 using MonoDragons.Core.UserInterface;
 
@@ -11,13 +14,17 @@ namespace MegaBuy.Jobs.ReturnSpecialist
     {
         private readonly ImageTextButton _purchaseHistory;
 
+        private bool _isInCall = false;
+
         public ClickUIBranch Branch { get; }
 
         public ReturnSpecialistUI()
         {
-            _purchaseHistory = ImageTextButtonFactory.Create("Purchases", new Vector2(700, 500), () => World.Publish(new AppChanged(App.PurchaseHistory)));
+            _purchaseHistory = ImageTextButtonFactory.Create("Purchases", new Vector2(700, 500), () => World.Publish(new AppChanged(App.PurchaseHistory)), () => _isInCall);
             Branch = new ClickUIBranch("Return Specialist", (int)ClickUIPriorities.Pad);
             Branch.Add(_purchaseHistory);
+            World.Subscribe(EventSubscription.Create<CallStarted>(x => _isInCall = true, this));
+            World.Subscribe(EventSubscription.Create<CallResolved>(x => _isInCall = false, this));
         }
 
         public void Draw(Transform2 parentTransform)
