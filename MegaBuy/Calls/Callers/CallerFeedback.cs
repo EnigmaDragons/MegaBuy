bb76610a49781c455c1baa55cc5a9350f1996d54
Silas Reinagel <silas.reinagel@gmail.com>
2017-06-15 19:57:22 -0700

    Added WorkReport data model. Closes #244

---
+++ MegaBuy/Calls/Callers/CallerFeedback.cs
@@ -0,0 +1,18 @@
+﻿using MegaBuy.Calls.Ratings;
+
+namespace MegaBuy.Calls.Callers
+{
+    public sealed class CallerFeedback
+    {
+        private CallRating _rating;
+
+        public int RatingScore => _rating.AsInt();
+        public string Review { get; private set; }
+
+        public CallerFeedback(CallRating rating, string review)
+        {
+            _rating = rating;
+            Review = review;
+        }
+    }
+}
---
+++ MegaBuy/Reports/WorkReportData.cs
@@ -0,0 +1,17 @@
+﻿using MegaBuy.Calls.Callers;
+
+namespace MegaBuy.Reports
+{
+    public sealed class WorkReportData
+    {
+        public float HoursWorkedActual { get; set; }
+        public float HoursWorkedExpected { get; set; }
+        public int CallsAnsweredActual { get; set; }
+        public int CallsAnsweredExpected { get; set; }
+        public float AvgCallRatingActual { get; set; }
+        public float AvgCallRatingExpected { get; set; }
+
+        public CallerFeedback BestFeedback { get; set; }
+        public CallerFeedback WorstFeedback { get; set; }
+    }
+}
